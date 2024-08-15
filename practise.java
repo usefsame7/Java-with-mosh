7d22cb42da66c83f12626b25d0cdcbec79784291
youssef <yousameh2006@gmail.com>
2024-08-15 16:09:45 +0300

    <>

---
+++ practise.java
@@ -0,0 +1,82 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class practise {
+
+
+  public static void main(String[] args) {
+    
+     Scanner scanner = new Scanner(System.in);
+     int p = scanner.nextInt();
+     float annualRate = scanner.nextFloat();
+ 
+     float r = annualRate / 100/ 12; 
+
+
+     float period = scanner.nextFloat();
+
+     float n = period * 12;
+
+     double k = Math.pow(1+r, n);
+
+     double mortgage = (p * r * k ) / (k - 1);
+
+     String result = NumberFormat.getCurrencyInstance().format(mortgage);
+   
+      System.out.println("mortgage: " + result);
+    
+
+
+
+
+      
+     // the ternary operator
+   
+      // the condition ? "if it's true": "false"
+
+
+   // ex
+
+  //    int money = 50;
+  //    String classDegree = money > 40 ? "first" : "Economy";
+  //  System.out.println(classDegree);
+
+
+
+
+
+
+
+
+  
+    // if the num is divisible by 5 --> return fizz 
+    // divisible by 3 --> buzz 
+    // divisible by 3&5 --> fizzbuzz
+ 
+  
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Number: ");
+    int num = scanner.nextInt();
+
+
+
+     if (num % 5 == 0 && num % 3 == 0) {
+        System.out.println("fizzbuzz");
+
+     } else {
+
+      if (num % 3 == 0) {
+        System.out.println("buzz");
+
+      } else if (num % 5 == 0) { 
+        System.out.println("fizz");
+       } else {
+        
+        System.out.println(num);
+       }
+   
+     }
+
+  
+ }
+}
